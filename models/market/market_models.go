9350ecfb16eae81bae2ca31dcc9c130ffc6bf10a
Ken Hsu <ken.hsu@varmeego.com>
2025-06-15 14:04:47 +0800

    feat: add Confirm field to Candle struct
    - Add Confirm boolean field to Candle struct for market data validation
    - Update UnmarshalJSON to parse confirm field from JSON array
    - Improve data integrity checking for candle data

--- models/market/market_models.go
+++ models/market/market_models.go
@@ -56,13 +56,14 @@ type (
 		OrderNumbers    int
 	}
 	Candle struct {
-		O      float64
-		H      float64
-		L      float64
-		C      float64
-		Vol    float64
-		VolCcy float64
-		TS     okex.JSONTime
+		O       float64
+		H       float64
+		L       float64
+		C       float64
+		Vol     float64
+		VolCcy  float64
+		TS      okex.JSONTime
+		Confirm bool
 	}
 	IndexCandle struct {
 		O       float64
@@ -136,10 +137,10 @@ func (o *OrderBookEntity) UnmarshalJSON(buf []byte) error {
 
 func (c *Candle) UnmarshalJSON(buf []byte) error {
 	var (
-		o, h, l, cl, vol, volCcy, ts string
-		err                          error
+		o, h, l, cl, vol, volCcy, ts, confirm string
+		err                                   error
 	)
-	tmp := []interface{}{&ts, &o, &h, &l, &cl, &vol, &volCcy}
+	tmp := []interface{}{&ts, &o, &h, &l, &cl, &vol, &volCcy, &confirm}
 	wantLen := len(tmp)
 	if err := json.Unmarshal(buf, &tmp); err != nil {
 		return err
@@ -181,6 +182,10 @@ func (c *Candle) UnmarshalJSON(buf []byte) error {
 	}
 
 	c.VolCcy, err = strconv.ParseFloat(volCcy, 64)
+	if err != nil {
+		return err
+	}
+	c.Confirm, err = strconv.ParseBool(confirm)
 	if err != nil {
 		return err
 	}
