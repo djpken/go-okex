1ec8527ccbcb8b67d085a57fd8f53da8a995ed43
Ken Hsu <ken.hsu@varmeego.com>
2025-06-15 16:53:13 +0800

    v1.0.6

--- models/market/market_models.go
+++ models/market/market_models.go
@@ -56,14 +56,15 @@ type (
 		OrderNumbers    int
 	}
 	Candle struct {
-		O       float64
-		H       float64
-		L       float64
-		C       float64
-		Vol     float64
-		VolCcy  float64
-		TS      okex.JSONTime
-		Confirm bool
+		O           float64
+		H           float64
+		L           float64
+		C           float64
+		Vol         float64
+		VolCcy      float64
+		VolCcyQuote float64
+		TS          okex.JSONTime
+		Confirm     bool
 	}
 	IndexCandle struct {
 		O       float64
@@ -137,10 +138,10 @@ func (o *OrderBookEntity) UnmarshalJSON(buf []byte) error {
 
 func (c *Candle) UnmarshalJSON(buf []byte) error {
 	var (
-		o, h, l, cl, vol, volCcy, ts, confirm string
-		err                                   error
+		o, h, l, cl, vol, volCcy, ts, volCcyQuote, confirm string
+		err                                                error
 	)
-	tmp := []interface{}{&ts, &o, &h, &l, &cl, &vol, &volCcy, &confirm}
+	tmp := []interface{}{&ts, &o, &h, &l, &cl, &vol, &volCcy, volCcyQuoute, &confirm}
 	wantLen := len(tmp)
 	if err := json.Unmarshal(buf, &tmp); err != nil {
 		return err
@@ -185,6 +186,11 @@ func (c *Candle) UnmarshalJSON(buf []byte) error {
 	if err != nil {
 		return err
 	}
+	c.VolCcyQuote, err = strconv.ParseFloat(volCcyQuote, 64)
+	if err != nil {
+		return err
+	}
+
 	c.Confirm, err = strconv.ParseBool(confirm)
 	if err != nil {
 		return err
